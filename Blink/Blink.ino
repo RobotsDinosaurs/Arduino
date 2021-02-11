bc2252c5f2a7bb41f65e0ece96147f0f638b50d8
RND <robots.dinosaurs.RND@gmail.com>
2021-02-11 11:49:37 -0500

    Added Blink code

---
+++ Blink/Blink.ino
@@ -0,0 +1,23 @@
+/*
+    Blink
+
+    Blink the built-in LED in 1 second intervals (On/Off).
+*/
+
+#include <Arduino.h>
+
+void setup() {
+    // initialize
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+    // turn the LED On
+    digitalWrite(LED_BUILTIN, HIGH);
+    // wait 1 second
+    delay(1000);
+    // turn the LED Off
+    digitalWrite(LED_BUILTIN, LOW);
+    // wait 1 second
+    delay(1000);
+}
