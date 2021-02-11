67eaafbbbb4c4b0e5f3a5c61f118706467d19639
RobotsDinosaurs <78386326+RobotsDinosaurs@users.noreply.github.com>
2021-02-11 17:26:26 -0500

    Create ServoWithPot.ino

---
+++ ServoWithPot/ServoWithPot.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+
+int potPin = 0;
+int servoPin = 9;
+int angle = 0;
+
+Servo servo;
+
+void setup() {
+  servo.attach(servoPin);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int reading = analogRead(potPin);     // 0 to 1023 (Arduino has a 10-bit analog to digital converter)
+  
+  angle = map(reading, 0, 1023, 0, 180);  // map 'reading' 
+                                          // from current range 0-1023
+                                          // to target range 0-180 degrees
+  Serial.println(angle);
+
+  servo.write(angle);
+  delay(15);
+}
