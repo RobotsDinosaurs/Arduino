f3ea11cca5a1c7a0ff8d2a17c269444582f4a111
RND <robots.dinosaurs.RND@gmail.com>
2021-03-10 22:25:48 -0500

    Added DaylightSavingTime and Alarm.

--- RTC/RTC.ino
+++ RTC/RTC.ino
@@ -1,7 +1,10 @@
 /*
     Real Time Clock Module DS1302
 
-    Displays date/time and RAM value on a serial monitor. 
+    Displays the current date/time in a 12-hour AM/PM format and checks alarm's date stored in RAM.
+    If alarm's time was passed, the alarm will be executed. 
+    Simple output to the serial monitor for simplicity. 
+    Automatically adjust for Daylight Savings Time and Standard Time for Eastern Time Zone (Toronto, New York, Detroit).
 
     Connections:
     * CLK/SCLK --> 5
@@ -12,7 +15,13 @@
 */
 #include <ThreeWire.h>  
 #include <RtcDS1302.h>
-#include <Timezone.h>
+
+// determine the size of an array dynamically
+#define countof(a) (sizeof(a) / sizeof(a[0]))
+
+// RtcDateTime uses Y2K as OriginYear, while time_t uses Epoch 1970 as OriginYear.
+// This is the difference between years 2000 and 1970 in seconds.
+#define 	UNIX_OFFSET   946684800
 
 const int RST_PIN   = 2;  // Chip Enable
 const int IO_PIN   = 4;   // Input/Output
@@ -21,65 +30,45 @@ const int SCLK_PIN = 5;   // Serial Clock
 ThreeWire myWire(IO_PIN,SCLK_PIN,RST_PIN);
 RtcDS1302<ThreeWire> Rtc(myWire);
 
-// determine the size of an array dynamically
-#define countof(a) (sizeof(a) / sizeof(a[0]))
+// 30 chars is max, RAM is 31 Bytes, last char is null.
+// Will be used to store the next alarm date+time.
+const char data[] = "";
 
-// 30 chars is max, RAM is 31 Bytes, last char is null
-const char data[] = "twinkle twinkle little star...";
+//Is Eastern Daylight Savings Time?
+bool isEDT = false;
 
-// RtcDateTime uses Y2K as OriginYear, while time_t uses Epoch 1970 as OriginYear
-// this is the difference between 2000 and 1970
-#define 	UNIX_OFFSET   946684800
-
-// US Eastern Time Zone (Toronto, New York, Detroit)
-TimeChangeRule myEDT = {"EDT", Second, Sun, Mar, 2, -240};    // Daylight time = UTC - 4 hours (Summer time)
-TimeChangeRule myEST = {"EST", First, Sun, Nov, 2, -300};     // Standard time = UTC - 5 hours
-Timezone myTZ(myEDT, myEST);
-
-// If TimeChangeRules are already stored in EEPROM, comment out the three
-// lines above and uncomment the line below.
-//Timezone myTZ(100);       // assumes rules stored at EEPROM address 100
-
-TimeChangeRule *tcr;        // pointer to the time change rule, use to get TZ abbrev
-
+// Set the current date and time for RTC based on compile time.
+// Calculate the next alarm date.
 void setup () 
 {
     Serial.begin(9600);
 
-    //Serial.print("compiled: ");
     //__DATE__ is a preprocessor macro that expands to current date (at compile time) 
     // in the form mmm dd yyyy (e.g. "Jan 14 2012"), as a string.
     const char* nowDate = __DATE__;
     const char* nowTime = __TIME__;
     
-    //Serial.print(nowDate);
-    //Serial.println(nowTime);
-    
-  // Set the date and time.  This only needs to be done once if there is a battery backup
-  // Format (Year, Month, Day, Hour (24 Format), Minute, Second, Day of Week)
-  // Update the following line to current date time the first time it is run then comment out
-  //Time t(2018, 7, 9, 16, 02, 00, Time::kMonday);
-  //rtc.time(t);
     Rtc.Begin();
+    runRtcChecks();
 
+    // Set the date and time.  This only needs to be done once if there is a battery backup
     RtcDateTime compiled = RtcDateTime(nowDate, nowTime);
-    printDateTime(compiled, "AAA");
-    //Serial.println();
+    Rtc.SetDateTime(compiled);    
 
-    //setTime(myTZ.toUTC(compiled.Epoch64Time()-UNIX_OFFSET));
-
-    runRtcChecks(compiled);
+    // Eastern Time Zone (Toronto, New York, Detroit)
+    // Daylight Saving Time is from Second Sunday in March after 2AM
+    // until First Sunday in November until 2AM
+    isEDT = isDaylightSavingTime(compiled);
     
-/* comment out on a second run to see that the info is stored long term */
-    storeDataInMemory(data, sizeof(data));
-/* end of comment out section */
+    storeAlarmInMemory(compiled);
 }
 
+// Print current time every 5 seconds. 
+// If current time passed the alarm time, execute Alarm and set the next alarm's date+time.
 void loop () 
 {
     RtcDateTime now = Rtc.GetDateTime();
-
-    printDateTime(now, "AAA");
+    RtcDateTime nextAlarm = readAlarmFromMemory();
 
     if (!now.IsValid())
     {
@@ -87,40 +76,54 @@ void loop ()
         //    1) the battery on the device is low or even missing and the power line was disconnected
         Serial.println("RTC lost confidence in the DateTime!");
     }
+    else 
+    {
+        // Adjust for DaylightSavingTime/Standard Time, Eastern Time Zone
+        if (now.Month() == 3 && !isEDT && isDaylightSavingTime(now))
+        {
+            // from EST to EDT (+ 1 hour)
+            now+=3600;
+            Rtc.SetDateTime(now);
 
-    time_t utc = myTZ.toUTC(now.Epoch64Time() - UNIX_OFFSET);
-    time_t local = myTZ.toLocal(utc, &tcr);
-    Serial.println();
-    printDateTime(utc, "UTC");
-    printDateTime(local, tcr -> abbrev);
-  
-    delay(5000);
+            nextAlarm+=3600;
+            storeAlarmInMemory(nextAlarm);
 
-    readDataFromMemory();
+            isEDT = true;
+        }
+        else if (now.Month() == 11 && isEDT && !isDaylightSavingTime(now)) 
+        {
+            // from EDT to EST (- 1 hour)
+            now-=3600;
+            Rtc.SetDateTime(now);
+
+            nextAlarm-=3600;
+            storeAlarmInMemory(nextAlarm);
+        
+            isEDT = false;
+        }
     
+        // debug
+        Serial.print("Next Alarm: ");
+        printDateTime(nextAlarm);
+
+        // execute alarm
+        if (now >= nextAlarm){
+            Serial.println("!!!!  ALARM  !!!!");
+            storeAlarmInMemory(nextAlarm);
+        }
+
+        // debug
+        Serial.print("Time now:   ");
+        printDateTime(now);
+    }
+
     delay(5000);
 }
 
-// format and print a time_t value, with a time zone appended.
-/*void printDateTimeTZ(time_t t, const char *tz)
+// Print date+time in a 12-hour AM/PM format.
+void printDateTime(const RtcDateTime& dt)
 {
-    char buf[32];
-    //Serial.println(buf);
-    char m[4];    // temporary storage for month string (DateStrings.cpp uses shared buffer)
-    //Serial.println(m);
-    strcpy(m, monthShortStr(month(t)));
-    Serial.println(t);
-    Serial.println(month(t));
-    Serial.println(day(t));
-    Serial.println(m);
-    sprintf(buf, "%.2d:%.2d:%.2d %s %.2d %s %d %s",
-        hour(t), minute(t), second(t), dayShortStr(weekday(t)), day(t), m, year(t), tz);
-    Serial.println(buf);
-}*/
-
-void printDateTime(const RtcDateTime& dt, const char format[4])
-{
-    char datestring[28];
+    char datestring[30];
 
     bool isPM = (dt.Hour() > 12);
     // read the format string from the Flash memory
@@ -128,30 +131,23 @@ void printDateTime(const RtcDateTime& dt, const char format[4])
             countof(datestring),
             // PSTR macro = pointer to a string 
             // creating a PROGMEM array and returning its address
-            PSTR("%02u:%02u:%02u %s %02u %s %04u %s"),
+            PSTR("%02u %s %04u %02u:%02u:%02u %s %s %s"),
+            dt.Day(),
+            monthShortString(dt.Month()),
+            dt.Year(),
             //Sutract 12, Exception: 12 AM = 00 in 24 hrs
             isPM ? dt.Hour()-12 : (dt.Hour() == 0 ? 12 : dt.Hour()),
             dt.Minute(),
             dt.Second(),
             isPM ? "PM" : "AM", 
-            dt.Day(),
-            monthShortStr(dt.Month()),
-            dt.Year(), 
-            format); 
+            "+", 
+            isEDT ? "EDT":"EST"); // Eastern Daylight Time/Eastern Standard Time
     Serial.println(datestring);
 }
 
-void runRtcChecks(RtcDateTime compiled)
+// Initialize chip by turning off write protection and clearing the clock halt flag.
+void runRtcChecks()
 {
-     // Initialize chip by turning off write protection and clearing the clock halt flag.
-    //Rtc.writeProtect(false);
-    //Rtc.halt(false);
-    if (!Rtc.IsDateTimeValid()) 
-    {
-        Serial.println("RTC lost confidence in the DateTime!");
-        Rtc.SetDateTime(compiled);
-    }
-
     if (Rtc.GetIsWriteProtected())
     {
         Serial.println("RTC was write protected, enabling writing now");
@@ -163,20 +159,34 @@ void runRtcChecks(RtcDateTime compiled)
         Serial.println("RTC was not actively running, starting now");
         Rtc.SetIsRunning(true);
     }
-
-    RtcDateTime now = Rtc.GetDateTime(); // always gets 24-hour format
-    if (now < compiled) 
-    {
-        Serial.println("RTC is older than compile time!  (Updating DateTime)");
-        Rtc.SetDateTime(compiled);
-    }
 }
 
-void storeDataInMemory(const char _data[], uint8_t _count) 
+// Calculate the next alarm's date and store in memory.
+void storeAlarmInMemory(RtcDateTime lastAlarm) 
 {
-    // Store something in memory on the RTC
-    //uint8_t count = sizeof(_data);
-    uint8_t written = Rtc.SetMemory((const uint8_t*)_data, _count); // this includes a null terminator for the string
+    // Calculate next alarm date 
+    // by rounding off to the last minute
+    RtcDateTime nextAlarm = RtcDateTime(lastAlarm.Year(), lastAlarm.Month(), lastAlarm.Day(), lastAlarm.Hour(), lastAlarm.Minute(), 0);
+    // then adding +1 min
+    nextAlarm = RtcDateTime(nextAlarm.Epoch64Time()-UNIX_OFFSET + 60);
+    char datestring[28];
+
+    // read the format string from the Flash memory
+    snprintf_P(datestring, 
+            countof(datestring),
+            // PSTR macro = pointer to a string 
+            // creating a PROGMEM array and returning its address
+            PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
+            nextAlarm.Day(),
+            nextAlarm.Month(),
+            nextAlarm.Year(),
+            nextAlarm.Hour(),
+            nextAlarm.Minute(),
+            nextAlarm.Second()); 
+
+    // Store next alarm in memory on the RTC
+    uint8_t _count = sizeof(datestring);
+    uint8_t written = Rtc.SetMemory((const uint8_t*)datestring, _count); // this includes a null terminator for the string
     if (written != _count) 
     {
         Serial.print("something didn't match, count = ");
@@ -187,9 +197,11 @@ void storeDataInMemory(const char _data[], uint8_t _count)
     }
 }
 
-void readDataFromMemory() {
+RtcDateTime readAlarmFromMemory() {
     // read data
     uint8_t buff[31];
+    char buffy[31];
+
     const uint8_t count = sizeof(buff);
     // get our data
     uint8_t gotten = Rtc.GetMemory(buff, count);
@@ -203,22 +215,68 @@ void readDataFromMemory() {
         Serial.println();
     }
 
-    Serial.print("data read (");
-    Serial.print(gotten);
-    Serial.print(") = \"");
     // print the string, but terminate if we get a null
     for (uint8_t ch = 0; ch < gotten && buff[ch]; ch++)
     {
-        Serial.print((char)buff[ch]);
+        buffy[ch] = buff[ch];
     }
-    Serial.println("\"");    
+
+    uint16_t day, month, year, hour, minute, second;
+    sscanf(buffy, "%02u/%02u/%04u %02u:%02u:%02u",
+    &day,
+    &month,
+    &year, 
+    &hour, 
+    &minute,
+    &second);
+
+    RtcDateTime nextAlarm = RtcDateTime(year, month, day, hour, minute, second);
+    return nextAlarm;
 }
 
 const char* monthShortString(uint8_t mth) {
     
     // Initialize array of pointer
-    const char *months[13] = { "", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
-    Serial.println(mth);
-    Serial.println(months[mth]);
+    const char *months[13] = { "Nil", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
     return months[mth];
 }
+
+// Adapted from https://github.com/SensorsIot/NTPtimeESP/blob/master/NTPtimeESP.cpp
+boolean isDaylightSavingTime(RtcDateTime _tempDateTime) {
+	// DST begins on second Sunday of March and ends on first Sunday of November. 
+	// Time change occurs at 2AM locally
+	if (_tempDateTime.Month() < 3 || _tempDateTime.Month() > 11) return false;  //January, february, and december are out.
+	if (_tempDateTime.Month() > 3 && _tempDateTime.Month() < 11) return true;   //April to October are in
+	int previousSunday = _tempDateTime.Day() - (_tempDateTime.DayOfWeek());  
+	
+    // -------------------- March ---------------------------------------
+	//In march, we are DST if our previous Sunday was = to or after the 8th.
+	if (_tempDateTime.Month() == 3 ) {  // in march, if previous Sunday is after the 8th, is DST
+		// unless Sunday and hour < 2am
+		if ( previousSunday >= 8 ) { 
+			// return true if day > 14 or (dow == 0 and hour >= 2)
+			return ((_tempDateTime.Day() > 14) || 
+			((_tempDateTime.DayOfWeek() == 0 && _tempDateTime.Hour() >= 2) || _tempDateTime.DayOfWeek() > 0));
+		}
+		else
+		{
+			// previousSunday has to be < 8 to get here
+			return false;
+		} 
+	} 
+
+	// ------------------------------- November -------------------------------
+	// gets here only if month = November
+	//In november we must be before the first Sunday to be dst.
+	//That means the previous Sunday must be before the 2nd.
+	if (previousSunday < 1)
+	{
+		// is not true for Sunday after 2am or any day after 1st Sunday any time
+		return ((_tempDateTime.DayOfWeek() == 0 && _tempDateTime.Hour() < 2) || (_tempDateTime.DayOfWeek() > 0));
+	} 
+	else
+	{
+		// return false unless after first wk and dow = Sunday and hour < 2
+		return (_tempDateTime.Day() <8 && _tempDateTime.DayOfWeek() == 0 && _tempDateTime.Hour() < 2);
+	}  
+} 
